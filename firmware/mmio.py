c2699b716ed3c627ad7f72680cec7efa0f834398
Peter van Tol <Peter.van.Tol@witteveenbos.com>
2022-04-06 14:59:17 +0000

    Reorder MMIO, group RW and RO registers

--- firmware/mmio.py
+++ firmware/mmio.py
@@ -14,11 +14,31 @@ from .pwm import PWM
 class MMIO(Module, AutoCSR):
 
     def __init__(self, gpio_in: List[GPIO], gpio_out: List[GPIO], pwm: List[PWM]):
-        # GPIO
-        self.gpio_in = CSRStatus(size=int(math.ceil(float(len(gpio_in))/32))*32, description="gpio_in", name='gpio_in')
+        """
+        Initializes the memory registers.
+
+        NOTE:
+        The order of the registers in the memory is in the same order as defined in
+        this class. This can be inspected by reviewing the generated csr.csv. The driver
+        expects the information blocks in the following order:
+        - READ:
+          - GPIO;
+          - PWM;
+        - WRITE:
+          - GPIO;
+
+        When the order of the MMIO is mis-aligned with respect to the driver this might
+        lead to errors (writing to the wrong registers) or the FPGA being hung up (when
+        writing to a read-only register).
+        """
+        # OUTPUT
+        # - GPIO
         self.gpio_out = CSRStorage(size=int(math.ceil(float(len(gpio_out))/32))*32, description="gpio_out", name='gpio_out', write_from_dev=False)
-        # PWM
+        # - PWM
         for index, _ in enumerate(pwm):
             setattr(self, f'pwm_{index}_enable', CSRStorage(size=32, description=f'pwm_{index}_enable', name=f'pwm_{index}_enable', write_from_dev=False))
             setattr(self, f'pwm_{index}_period', CSRStorage(size=32, description=f'pwm_{index}_period', name=f'pwm_{index}_period', write_from_dev=False))
             setattr(self, f'pwm_{index}_width', CSRStorage(size=32, description=f'pwm_{index}_width', name=f'pwm_{index}_width', write_from_dev=False))
+
+        # INPUT
+        self.gpio_in = CSRStatus(size=int(math.ceil(float(len(gpio_in))/32))*32, description="gpio_in", name='gpio_in')
