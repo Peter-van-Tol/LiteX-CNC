dc54293e4e7993ae223ce3cabb9a847963c52a27
Peter van Tol <Peter.van.Tol@witteveenbos.com>
2022-04-25 12:52:06 +0000

    Stable stepgen for standing still

--- driver/stepgen.c
+++ driver/stepgen.c
@@ -4,6 +4,15 @@
 #include "litexcnc.h"
 #include "stepgen.h"
 
+// Declaration of parameters used in litexcnc_stepgen_prepare_write
+double max_speed_desired;
+double speed_new;
+double speed_avg;
+double match_time;
+double pos_avg;
+double pos_error;
+
+
 
 int litexcnc_stepgen_init(litexcnc_t *litexcnc, json_object *config) {
 
@@ -60,10 +69,10 @@ int litexcnc_stepgen_init(litexcnc_t *litexcnc, json_object *config) {
             r = hal_param_u32_newf(HAL_RW, &(instance->hal.param.stepspace), litexcnc->fpga->comp_id, "%s.stepspace", base_name);
             if (r != 0) { goto fail_params; }
             // - Timing - dirsetup
-            r = hal_param_u32_newf(HAL_RW, &(instance->hal.param.dirsetup), litexcnc->fpga->comp_id, "%s.dirsetup", base_name);
+            r = hal_param_u32_newf(HAL_RW, &(instance->hal.param.dir_setup_time), litexcnc->fpga->comp_id, "%s.dir-setup-time", base_name);
             if (r != 0) { goto fail_params; }
             // - Timing - dirhold
-            r = hal_param_u32_newf(HAL_RW, &(instance->hal.param.dirhold), litexcnc->fpga->comp_id, "%s.dirhold", base_name);
+            r = hal_param_u32_newf(HAL_RW, &(instance->hal.param.dir_hold_time), litexcnc->fpga->comp_id, "%s.dir-hold-time", base_name);
             if (r != 0) { goto fail_params; }
 
             // Create the pins
@@ -71,7 +80,16 @@ int litexcnc_stepgen_init(litexcnc_t *litexcnc, json_object *config) {
             r = hal_pin_u32_newf(HAL_OUT, &(instance->hal.pin.counts), litexcnc->fpga->comp_id, "%s.counts", base_name);
             if (r != 0) { goto fail_pins; }
             // - position_fb
-            r = hal_pin_float_newf(HAL_OUT, &(instance->hal.pin.position_fb), litexcnc->fpga->comp_id, "%s.position-fb", base_name);
+            r = hal_pin_float_newf(HAL_OUT, &(instance->hal.pin.position_fb), litexcnc->fpga->comp_id, "%s.position-feedback", base_name);
+            if (r != 0) { goto fail_pins; }
+            // - position_prediction
+            r = hal_pin_float_newf(HAL_OUT, &(instance->hal.pin.position_prediction), litexcnc->fpga->comp_id, "%s.position_prediction", base_name);
+            if (r != 0) { goto fail_pins; }
+            // - speed_fb
+            r = hal_pin_float_newf(HAL_OUT, &(instance->hal.pin.speed_fb), litexcnc->fpga->comp_id, "%s.speed-feedback", base_name);
+            if (r != 0) { goto fail_pins; }
+            // - speed_prediction
+            r = hal_pin_float_newf(HAL_OUT, &(instance->hal.pin.speed_prediction), litexcnc->fpga->comp_id, "%s.speed-prediction", base_name);
             if (r != 0) { goto fail_pins; }
             // - enable
             r = hal_pin_bit_newf(HAL_IN, &(instance->hal.pin.enable), litexcnc->fpga->comp_id, "%s.enable", base_name);
@@ -84,6 +102,10 @@ int litexcnc_stepgen_init(litexcnc_t *litexcnc, json_object *config) {
             if (r != 0) { goto fail_pins; }
         }
     }
+
+    // Pre-calculate the amount of cycles per nanp-second. Required to convert the 
+    // timings to amount of clock-cycles
+    litexcnc->stepgen.data.cycles_per_ns = litexcnc->clock_frequency * 1E-9; 
 
     return r;
 
@@ -103,12 +125,232 @@ fail_pins:
 }
 
 
-uint8_t litexcnc_stepgen_prepare_write(litexcnc_t *litexcnc, uint8_t **data) {
+uint8_t litexcnc_stepgen_prepare_write(litexcnc_t *litexcnc, uint8_t **data, long period) {
 
+    // Check whether the period of the thread has changed
+    if (litexcnc->stepgen.memo.period != period) { 
+        // - Calculate the reciprocal once here, to avoid multiple divides later
+        litexcnc->stepgen.data.recip_dt = 1.0 / (period * 0.000000001);
+        litexcnc->stepgen.memo.period = period;
+    }
+
+    // STEP 1: Timings
+    // ===============
+    // Check whether any of the parameter of the stepgens has changed. All stepgens
+    // will use the same values for steplen, dir_hold_time and dir_setup_time. The
+    // maximum value is governing, so we start with the value 0. For each instance
+    // it is checked whether the value has changed. If it has changed, the time is
+    // converted to cycles.
+    // NOTE: all timings are in nano-seconds (1E-9), so the timing is multiplied with
+    // the clock-frequency and divided by 1E9. However, this might lead to issues
+    // with roll-over of the 32-bit integer. 
+    litexcnc_stepgen_general_write_data_t data_general;
+    uint32_t stepspace_cycles; // Separate variable, as it is not part of the data-package
+    for (size_t i=0; i<litexcnc->stepgen.num_instances; i++) {
+        // Get pointer to the stepgen instance
+        litexcnc_stepgen_pin_t *instance = &(litexcnc->stepgen.instances[i]);
+
+        // Recalculate the timings
+        // - steplen
+        if (instance->hal.param.steplen != instance->memo.steplen) {
+            instance->data.steplen_cycles = instance->hal.param.steplen * litexcnc->stepgen.data.cycles_per_ns + 0.5;
+            instance->memo.steplen = instance->hal.param.steplen; 
+        }
+        if (instance->data.steplen_cycles > data_general.steplen) {data_general.steplen = instance->data.steplen_cycles;};
+        // - stepspace
+        if (instance->hal.param.stepspace != instance->memo.stepspace) {
+            instance->data.stepspace_cycles = instance->hal.param.stepspace * litexcnc->stepgen.data.cycles_per_ns + 0.5;
+            instance->memo.stepspace = instance->hal.param.stepspace; 
+        }
+        if (instance->data.stepspace_cycles > stepspace_cycles) {stepspace_cycles = instance->data.stepspace_cycles;};
+        // - dir_hold_time
+        if (instance->hal.param.dir_hold_time != instance->memo.dir_hold_time) {
+            instance->data.dirhold_cycles = instance->hal.param.dir_hold_time * litexcnc->stepgen.data.cycles_per_ns + 0.5;
+            instance->memo.dir_hold_time = instance->hal.param.dir_hold_time; 
+        }
+        if (instance->data.dirhold_cycles > data_general.dir_hold_time) {data_general.dir_hold_time = instance->data.dirhold_cycles;};
+        // - dir_setup_time
+        if (instance->hal.param.dir_setup_time != instance->memo.dir_setup_time) {
+            instance->data.dirsetup_cycles = instance->hal.param.dir_setup_time * litexcnc->stepgen.data.cycles_per_ns + 0.5;
+            instance->memo.dir_setup_time = instance->hal.param.dir_setup_time; 
+        }
+        if (instance->data.dirsetup_cycles > data_general.dir_setup_time) {data_general.dir_setup_time = instance->data.dirsetup_cycles;};
+    }
+
+    // Calculate the maximum frequency for stepgen
+    litexcnc->stepgen.data.max_frequency = (double) litexcnc->clock_frequency / (data_general.steplen + stepspace_cycles);
+
+    // Determine the apply time
+    // TODO: should be based on the wall-clock + this period converted to cycles
+
+    // Convert the general data to the correct byte order
+    data_general.steplen = htobe32(data_general.steplen);
+    data_general.dir_hold_time = htobe32(data_general.dir_hold_time);
+    data_general.dir_setup_time = htobe32(data_general.dir_setup_time);
+    data_general.apply_time = htobe64(data_general.apply_time);
+
+    // Put the data on the data-stream and advance the pointer
+    memcpy(*data, &data_general, LITEXCNC_STEPGEN_GENERAL_WRITE_DATA_SIZE);
+    *data += LITEXCNC_STEPGEN_GENERAL_WRITE_DATA_SIZE;
+
+
+    // STEP 2: Speed per stepgen
+    // =========================
+    double desired_speed;
+    for (size_t i=0; i<litexcnc->stepgen.num_instances; i++) {
+        litexcnc_stepgen_instance_write_data_t data_instance;
+        // Get pointer to the stepgen instance
+        litexcnc_stepgen_pin_t *instance = &(litexcnc->stepgen.instances[i]);
+
+        // Check the limits on the speed of the setpgen
+        if (instance->hal.param.maxvel <= 0.0) {
+            // Negative speed is limited as zero
+            instance->hal.param.maxvel = 0.0;
+        } else {
+            // Maximum speed is positive and no zero, compare with maximum frequency
+            max_speed_desired = instance->hal.param.maxvel;
+	        if (max_speed_desired > (litexcnc->stepgen.data.max_frequency * fabs(instance->hal.param.position_scale))) {
+                instance->hal.param.maxvel = litexcnc->stepgen.data.max_frequency * fabs(instance->hal.param.position_scale);
+		        // Maximum speed is too high, complain about it and modify the value
+		        if (!instance->memo.error_max_speed_printed) {
+		            LITEXCNC_ERR_NO_DEVICE(
+			            "STEPGEN: Channel %d: The requested maximum velocity of %.2f units/sec is too high.\n",
+			            i, max_speed_desired);
+		            LITEXCNC_ERR_NO_DEVICE(
+			            "STEPGEN: The maximum possible velocity is %.2f units/second\n",
+			            instance->hal.param.maxvel);
+		            instance->memo.error_max_speed_printed = true;
+		        }
+	        }
+        }
+
+        // NOTE: The acceleration is not limited by this component. The physical maximum 
+        // acceleration is applied within the FPGA
+        if (instance->hal.param.position_mode) {
+            // Position mode (currently the only supported method)
+            // Estimate the required (average) speed for getting to the commanded position, required time to
+            // accelerate to that speed.
+            speed_avg = (*(instance->hal.pin.position_cmd) - *(instance->hal.pin.position_prediction)) * litexcnc->stepgen.data.recip_dt;
+            match_time = fabs(speed_avg - *(instance->hal.pin.speed_prediction)) / instance->hal.param.maxaccel;
+            if (match_time < period) {
+                // The average speed can be obtained during one period. Now determine at which 
+                // position we would end up if this average speed was used. There will be a difference 
+                // between the commanded position and the 'average' position we have to compensate for.
+                pos_avg = *(instance->hal.pin.position_prediction) \
+                        + 0.5 * ( *(instance->hal.pin.speed_prediction) + speed_avg) * match_time \
+                        + (period - match_time) * speed_avg;
+                pos_error = *(instance->hal.pin.position_cmd) - pos_avg;
+                // Determine which the 'extra' speed required to solve for this position error. 
+                // The error can only be fully compensated when the match_time is less then half
+                // of the period, otherwise the acceleration constraint cannot be met.
+                if (match_time > 0.5 * period) {
+                    match_time = 0.5 * period;
+                }
+                speed_new = speed_avg + pos_error * 2 / match_time; 
+            } else {
+                // The whole period we must accelerate to even get to the average speed. Apply 
+                // maximum acceleration. If this happens multiple periods in a row, this might
+                // get into following errors.
+                speed_new = speed_avg;
+            }
+        } else {
+            // Speed mode
+            speed_new = *(instance->hal.pin.velocity_cmd);
+        }
+        // Limit the speed to the maximum speed
+        if (speed_new > instance->hal.param.maxvel) {
+            speed_new = instance->hal.param.maxvel;
+        } else if (speed_new < -1 * instance->hal.param.maxvel) {
+            speed_new = -1 * instance->hal.param.maxvel;
+        }
+        // Now the new speed has been calculated, convert it from units/s to steps per clock-cycle
+        data_instance.speed_target = htobe32((speed_new * instance->hal.param.position_scale * litexcnc->clock_frequency_recip) + 0x80000000);
+        data_instance.speed_target = htobe32(0x80000000);
+        data_instance.max_acceleration = htobe32(instance->hal.param.maxaccel * instance->hal.param.position_scale * litexcnc->clock_frequency_recip);
+        // Put the data on the data-stream and advance the pointer
+        memcpy(*data, &data_instance, LITEXCNC_STEPGEN_INSTANCE_WRITE_DATA_SIZE);
+        *data += LITEXCNC_STEPGEN_INSTANCE_WRITE_DATA_SIZE;
+    }
 }
 
 
-uint8_t litexcnc_stepgen_process_read(litexcnc_t *litexcnc, uint8_t** data) {
+uint8_t litexcnc_stepgen_process_read(litexcnc_t *litexcnc, uint8_t** data, long period) {
 
+    // Receive and process the data for all the stepgens
+    for (size_t i=0; i<litexcnc->stepgen.num_instances; i++) {
+        // Get pointer to the stepgen instance
+        litexcnc_stepgen_pin_t *instance = &(litexcnc->stepgen.instances[i]);
 
+        // Recalculate the reciprocal of the position scale if it has changed
+        if (instance->hal.param.position_scale != instance->memo.position_scale) {
+            // Prevent division by zero
+            if ((instance->hal.param.position_scale > -1e-20) && (instance->hal.param.position_scale < 1e-20)) {
+		        // Value too small, take a safe value
+		        instance->hal.param.position_scale = 1.0;
+	        }
+            instance->data.scale_recip = (1.0 / (1L << PICKOFF)) / instance->hal.param.position_scale;
+            instance->memo.position_scale = instance->hal.param.position_scale; 
+        }
+
+        // Store the old data
+        instance->memo.position = instance->data.position;
+        // Read data and proceed the buffer
+        int64_t pos;
+        memcpy(&pos, *data, sizeof pos);
+        instance->data.position = be64toh(pos);
+        LITEXCNC_PRINT_NO_DEVICE("Pos feedback %d \n", pos);
+        *data += 8;  // The data read is 64 bit-wide. The buffer is 8-bit wide
+        uint32_t speed;
+        memcpy(&speed, *data, sizeof speed);
+        instance->data.speed = (int64_t) be32toh(speed) -  0x80000000;
+        LITEXCNC_PRINT_NO_DEVICE("Speed feedback %d \n", instance->data.speed);
+        *data += 4;  // The data read is 32 bit-wide. The buffer is 8-bit wide
+        // Convert the received position to HAL pins for counts and floating-point position
+        *(instance->hal.pin.counts) = instance->data.position >> PICKOFF;
+        // Check: why is a half step subtracted from the position. Will case a possible problem 
+        // when the power is cycled -> will lead to a moving reference frame  
+        *(instance->hal.pin.position_fb) = (double)(instance->data.position-(1<<(PICKOFF-1))) * instance->data.scale_recip;
+        *(instance->hal.pin.speed_fb) = (double) litexcnc->clock_frequency * instance->data.speed * instance->data.scale_recip;
+
+        /* -------------------
+         * Predict the position and speed at the theoretical end of the start of the 
+         * update period. The prediction is based on:
+         *    - if there is a pending apply time (apply_time > wall_clock) the movement until that
+         *      apply time based on the position, speed and acceleration as read from the FPGA.
+         *    - any movement (with respect to speed and acceleration) which happens until the next
+         *      apply time, which is typically equal to the period of the function.
+         *
+         * This function is placed under read, as it uses the output from the previous cycle. If this
+         * was to be placed under the write cycle, errors might occur if the input variables such
+         * as the acceleration would change between read and write.
+         * ------------------- 
+         */
+        // - start with the current speed and position
+        *(instance->hal.pin.speed_prediction) = *(instance->hal.pin.speed_fb);
+        *(instance->hal.pin.position_prediction) =  *(instance->hal.pin.position_fb);
+        // - pending apply time
+        // TODO: expected to be non-relevant though
+        // - check whether the difference in target speed and current speed can be bridged 
+        //   within 1 period.
+	    double match_time = fabs(*(instance->hal.pin.speed_fb) - *(instance->hal.pin.velocity_cmd)) / instance->hal.param.maxaccel;
+        if (match_time <= period) {
+            // The predicted speed at the end of the period is equal to the commanded velocity
+            *(instance->hal.pin.speed_prediction) = *(instance->hal.pin.velocity_cmd);
+            // The position is calculated based on the average speed during the acceleration fase (the match_time) and 
+            // commanded speed during the constant speed fase
+            *(instance->hal.pin.position_prediction) +=  0.5 * (*(instance->hal.pin.speed_fb) + *(instance->hal.pin.speed_prediction)) * (match_time * 0.000000001);
+            *(instance->hal.pin.position_prediction) +=  *(instance->hal.pin.speed_prediction) * ((period - match_time) * 0.000000001);
+        } else {
+            // The speed is equal to the current velocity + acceleration / deceleration
+            if (*(instance->hal.pin.speed_fb) < *(instance->hal.pin.velocity_cmd)) {
+                // Acceleration, current speed is lower then the commanded speed
+                *(instance->hal.pin.speed_prediction) += (period * 0.000000001) * instance->hal.param.maxaccel;
+            } else {
+                // Deceleration, current speed is larger then the commanded speed
+                *(instance->hal.pin.speed_prediction) -= (period * 0.000000001) * instance->hal.param.maxaccel;
+            }
+            // The position can be determined based on the average speed (area under the trapezoidal speed curve)
+            *(instance->hal.pin.position_prediction) +=  0.5 * (*(instance->hal.pin.speed_fb) + *(instance->hal.pin.speed_prediction)) * (period * 0.000000001);
+        }
+    }
 }
--- driver/stepgen.h
+++ driver/stepgen.h
@@ -38,16 +38,22 @@
 
 #include "litexcnc.h"
 
+#define PICKOFF 28 
+#define POSITION_MODE 0
+
 // Defines the structure of the PWM instance
 typedef struct {
     struct {
 
         struct {
-            hal_u32_t *counts;         /* The current position, in counts */
-            hal_float_t *position_fb;  /* The current position, in length units (see parameter position-scale). The resolution of position-fb is much finer than a single step. If you need to see individual steps, use counts. */ 
-            hal_bit_t *enable;         /* Enables output steps - when false, no steps are generated and is the hardware disabled */
-            hal_float_t *velocity_cmd; /* Commanded velocity, in length units per second (see parameter position-scale). */
-            hal_float_t *position_cmd; /* Commanded position, in length units (see parameter position-scale). */ 
+            hal_u32_t *counts;                /* The current position, in counts */
+            hal_float_t *position_fb;         /* The current position, in length units (see parameter position-scale). The resolution of position-fb is much finer than a single step. If you need to see individual steps, use counts. */ 
+            hal_float_t *position_prediction; /* The predicted position, in length units (see parameter position-scale), at the start of the next position command execution */ 
+            hal_float_t *speed_fb;            /* The current speed, in length units per second (see parameter position-scale). */ 
+            hal_float_t *speed_prediction;    /* The predicted speed, in length units per second (see parameter position-scale), at the start of the next position command execution */ 
+            hal_bit_t *enable;                /* Enables output steps - when false, no steps are generated and is the hardware disabled */
+            hal_float_t *velocity_cmd;        /* Commanded velocity, in length units per second (see parameter position-scale). */
+            hal_float_t *position_cmd;        /* Commanded position, in length units (see parameter position-scale). */ 
         } pin;
 
         struct {
@@ -57,21 +63,35 @@ typedef struct {
             hal_float_t position_scale; /* The scaling for position feedback, position command, and velocity command, in steps per length unit. */ 
             hal_u32_t steplen;          /* The length of the step pulses, in nanoseconds. Measured from rising edge to falling edge. */
             hal_u32_t stepspace;        /* The minimum space between step pulses, in nanoseconds. Measured from falling edge to rising edge. The actual time depends on the step rate and can be much longer. Is used to calculate the maximum stepping frequency */ 
-            hal_u32_t dirsetup;         /* The minimum setup time from direction to step, in nanoseconds periods. Measured from change of direction to rising edge of step. */
-            hal_u32_t dirhold;          /* The minimum hold time of direction after step, in nanoseconds. Measured from falling edge of step to change of direction */
+            hal_u32_t dir_setup_time;   /* The minimum setup time from direction to step, in nanoseconds periods. Measured from change of direction to rising edge of step. */
+            hal_u32_t dir_hold_time;    /* The minimum hold time of direction after step, in nanoseconds. Measured from falling edge of step to change of direction */
+            hal_bit_t position_mode;    /* Flag indicating this stepgen should work in position mode */
         } param;
 
     } hal;
 
     // This struct holds all old values (memoization) 
     struct {
+        int64_t position;
         hal_float_t position_cmd;
         hal_float_t position_scale;
         hal_u32_t steplen;
         hal_u32_t stepspace;
-        hal_u32_t dirsetup;
-        hal_u32_t dirhold;  
+        hal_u32_t dir_setup_time;
+        hal_u32_t dir_hold_time;  
+        bool error_max_speed_printed;
     } memo;
+
+    // This struct contains data, both calculated and direct received from the FPGA
+    struct {
+        int64_t position;
+        int32_t speed;
+        float scale_recip;
+        hal_u32_t steplen_cycles;
+        hal_u32_t stepspace_cycles;
+        hal_u32_t dirsetup_cycles;
+        hal_u32_t dirhold_cycles; 
+    } data;
     
 } litexcnc_stepgen_pin_t;
 
@@ -81,7 +101,17 @@ typedef struct {
     // Input pins
     int num_instances;
     litexcnc_stepgen_pin_t *instances;
+
+    struct {
+        long period;
+    } memo;
     
+    // Struct containing pre-calculated values
+    struct {
+        float cycles_per_ns; 
+        float max_frequency;
+        float recip_dt;
+    } data;
 
 } litexcnc_stepgen_t;
 
@@ -95,22 +125,24 @@ typedef struct {
     uint32_t dir_setup_time;
     uint64_t apply_time;
 } litexcnc_stepgen_general_write_data_t;
+#define LITEXCNC_STEPGEN_GENERAL_WRITE_DATA_SIZE sizeof(litexcnc_stepgen_general_write_data_t)
 typedef struct {
     uint32_t speed_target;
     uint32_t max_acceleration;
 } litexcnc_stepgen_instance_write_data_t;
-#define LITEXCNC_STEPGEN_INSTANCE_READ_DATA_SIZE sizeof(litexcnc_stepgen_instance_write_data_t)
-#define LITEXCNC_BOARD_STEPGEN_DATA_WRITE_SIZE(litexcnc) sizeof(litexcnc_stepgen_general_write_data_t) + LITEXCNC_STEPGEN_INSTANCE_READ_DATA_SIZE*litexcnc->stepgen.num_instances
+#define LITEXCNC_STEPGEN_INSTANCE_WRITE_DATA_SIZE sizeof(litexcnc_stepgen_instance_write_data_t)
+#define LITEXCNC_BOARD_STEPGEN_DATA_WRITE_SIZE(litexcnc) sizeof(litexcnc_stepgen_general_write_data_t) + LITEXCNC_STEPGEN_INSTANCE_WRITE_DATA_SIZE*litexcnc->stepgen.num_instances
 // - read
 typedef struct {
-    uint32_t position;
+    int64_t position;
+    uint32_t speed;
 } litexcnc_stepgen_instance_read_data_t;
 #define LITEXCNC_BOARD_STEPGEN_DATA_READ_SIZE(litexcnc) sizeof(litexcnc_stepgen_instance_read_data_t)*litexcnc->stepgen.num_instances // PWM does not send data back
 
 
 // Functions for creating, reading and writing stepgen pins
 int litexcnc_stepgen_init(litexcnc_t *litexcnc, json_object *config);
-uint8_t litexcnc_stepgen_prepare_write(litexcnc_t *litexcnc, uint8_t **data);
-uint8_t litexcnc_stepgen_process_read(litexcnc_t *litexcnc, uint8_t** data);
+uint8_t litexcnc_stepgen_prepare_write(litexcnc_t *litexcnc, uint8_t **data, long period);
+uint8_t litexcnc_stepgen_process_read(litexcnc_t *litexcnc, uint8_t** data, long period);
 
 #endif
