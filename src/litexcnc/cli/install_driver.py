8463a99ea5ed4026146de09d06abc45e2d93d3f8
Peter van Tol <Peter.van.Tol@witteveenbos.com>
2023-01-29 21:42:02 +0100

    Auto-detect modules and boards

--- src/litexcnc/cli/install_driver.py
+++ src/litexcnc/cli/install_driver.py
@@ -2,6 +2,7 @@
 This file contains the command to compile the driver
 
 """
+import glob
 import os
 import subprocess
 import sys
@@ -9,14 +10,39 @@ import click
 
 # Import the driver module.
 from litexcnc import driver
+from litexcnc.driver.halcompile import find_modinc
 
 @click.command()
 def cli():
+
+    # Find modules
+    click.echo(click.style("INFO", fg="blue") + ": Retrieving modules to compile...")
+    modules = []
+    # - default modules
+    for module in glob.glob(f"{os.path.dirname(os.path.abspath(driver.__file__))}/modules/litexcnc_*.c"):
+        module = module[len(f"{os.path.dirname(os.path.abspath(driver.__file__))}/"):]
+        click.echo(f"... found '{module}'")
+        modules.append(module)
+    # - external modules(TODO)
+    ...
+
+    # Find modules
+    click.echo(click.style("INFO", fg="blue") + ": Retrieving board drivers to compile...")
+    boards = []
+    # - default modules
+    for board in glob.glob(f"{os.path.dirname(os.path.abspath(driver.__file__))}/boards/litexcnc_*.c"):
+        board = board[len(f"{os.path.dirname(os.path.abspath(driver.__file__))}/"):]
+        click.echo(f"... found '{board}'")
+        boards.append(board)
+    # - external modules(TODO)
     
-    # compile the driver
+    print(f'{sys.executable} halcompile.py --install litexcnc.c stepgen/pos2vel.c {" ".join(modules)}  {" ".join(boards)}',)
+
+
+    # Compile the driver
     click.echo(click.style("INFO", fg="blue") + ": Compiling LitexCNC driver...")
     ret = subprocess.call(
-        f'{sys.executable} halcompile.py --install litexcnc.c litexcnc_eth.c stepgen/pos2vel.c',
+        f'{sys.executable} halcompile.py --install litexcnc.c stepgen/pos2vel.c {" ".join(modules)}  {" ".join(boards)}',
         cwd=os.path.dirname(os.path.abspath(driver.__file__)),
         shell=True
     )
@@ -26,6 +52,3 @@ def cli():
 
     # Done!
     click.echo(click.style("INFO", fg="blue") + ": LitexCNC driver installed")
-
-    
-
