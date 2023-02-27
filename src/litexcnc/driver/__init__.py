698c8dc7e90e526ff888e83ef2bcf857d89ce7f7
Peter van Tol <Peter.van.Tol@witteveenbos.com>
2023-02-27 22:10:54 +0100

    Excluded janked boards

--- src/litexcnc/driver/__init__.py
+++ src/litexcnc/driver/__init__.py
@@ -4,7 +4,12 @@ from pathlib import Path
 # list by hand, but this is not recommended.
 TYPES = ('**/*.c', '**/*.h') # the tuple of file types
 FILES = []
+EXCLUDE = [
+    'config.h',
+    'litexcnc_debug.c',
+    'litexcnc_debug.h'
+]
 for type_ in TYPES:
     for file in Path(__file__).parent.glob(type_):
-        if not file.name == 'config.h':
+        if not file.name in EXCLUDE:
             FILES.append(file)
