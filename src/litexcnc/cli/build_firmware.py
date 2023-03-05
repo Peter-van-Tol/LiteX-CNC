88fef26f989f2eb58213fe5044cdfa7ea48fdd93
Peter van Tol <petertgvantol@gmail.com>
2023-03-05 19:56:39 +0000

    Build alias file as part of build process

--- src/litexcnc/cli/build_firmware.py
+++ src/litexcnc/cli/build_firmware.py
@@ -8,7 +8,8 @@ import click
 @click.argument('config', type=click.File('r'))
 @click.option('-o', '--output-directory')
 @click.option('--build', is_flag=True)
-def cli(config, output_directory, build):
+@click.option('-a', '--alias-file', is_flag=True)
+def cli(config, output_directory, build, alias_file):
     """Builds the litexCNC firmware with the given configuration"""
     # Local imports, with check whether Litex is available on path
     try:
@@ -35,6 +36,16 @@ def cli(config, output_directory, build):
     # Load configuration
     firmware_config = LitexCNC_Firmware.parse_raw(' '.join(config.readlines()))
 
+    # Output the aliases
+    if alias_file:
+        # Create a collection with aliases
+        alias = []
+        for module in firmware_config.modules:
+            alias.extend(module.create_aliases(firmware_config.board_name))
+        # Write the alias to the output file
+        with open(os.path.join(output_directory, 'alias.hal'), 'wt') as alias_ouput:
+            print('\n'.join(alias), file=alias_ouput)
+
     # Generate the firmware
     soc = firmware_config.generate()
     
@@ -48,3 +59,6 @@ def cli(config, output_directory, build):
 
     # Done!
     click.echo(click.style("INFO", fg="blue") + f": Firmware created in {output_directory}")
+    if alias_ouput:
+        click.echo(click.style("INFO", fg="blue") + f": Alias HAL-file created in {os.path.join(output_directory, 'alias.hal')}")
+    
