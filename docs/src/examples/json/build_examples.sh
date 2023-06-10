effb4c81030cd0adff0a0b29f8f579f85f51efe5
Peter van Tol <petervantol@gmail.com>
2023-06-10 20:41:25 +0200

    Build examples

--- docs/src/examples/json/build_examples.sh
+++ docs/src/examples/json/build_examples.sh
@@ -1,4 +1,11 @@
 for f in ./*.json; 
 do 
-    litexcnc build_firmware $f -a --build; 
+    mkdir ./build/${f%.*}
+    litexcnc build_firmware $f -a --build -o build 2>&1 | tee -a ./build/${f%.*}/${f%.*}.log;
+    pattern=./build/${f%.*}/gateware/colorlight_5a_75?.bit
+    bit_file=($pattern)
+    svf_flash=${bit_file%.*}_flash.svf
+    litexcnc convert_bit_to_flash $bit_file ${bit_file%.*}_flash.svf 2>&1 | tee -a ./build/${f%.*}/${f%.*}.log
+    zip -r ./zipped/${f%.*}_FULL.zip $f ./build/${f%.*};
+    zip -r ./zipped/${f%.*}.zip $f ./build/${f%.*}/alias.hal ./build/${f%.*}/csr.csv ./build/${f%.*}/gateware/colorlight_5a_75?.svf ./build/${f%.*}/gateware/colorlight_5a_75?_flash.svf ./build/${f%.*}/${f%.*}.log;
 done
