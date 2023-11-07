7eaa19c56cddb255956571f033d447b0fddf7a7d
Peter van Tol <petertgvantol@gmail.com>
2023-11-07 20:44:36 +0100

    Include installing litex

--- src/litexcnc/cli/install_toolchain.py
+++ src/litexcnc/cli/install_toolchain.py
@@ -9,18 +9,46 @@ from pathlib import Path
 import click
 import requests
 
-@click.command()
-@click.option('--user', is_flag=True)
-@click.option(
-    '-a', '--architecture', 'arch',
-    type=click.Choice(['arm', 'arm64', 'x64'], case_sensitive=False),
-    help="Use specific architecture. Leave empty for auto-detect")
-@click.option(
-    '-os', '--os', 'os_',
-    type=click.Choice(['darwin', 'linux', 'windows'], case_sensitive=False),
-    help="Use specific os. Leave empty for auto-detect")
-def cli(user, arch, os_):
+
+def _install_litex(target: str, user: bool) -> int:
+    """Installs Litex from https://github.com/enjoy-digital/litex"""
+    target = os.path.join(target, 'litex')
+    
+    if not os.path.exists(target):
+        os.mkdir(target)
+
+    # Download the setup file
+    response = requests.get(
+        'https://raw.githubusercontent.com/enjoy-digital/litex/master/litex_setup.py'
+    )
+    open(os.path.join(target, 'litex_setup.py'), 'wb').write(response.content)
+
+    # Run the python file
+    command = "litex_setup.py --init --install --config=standard --tag=2022.04"
+    if user:
+        command += " --user"
+    command = f'{sys.executable} {os.path.join(target, command)}'  #  --gcc=riscv if SOC with cpu
+    click.echo(click.style("INFO", fg="blue") + f": Installing Litex with the command '{command}'.")
+    ret = subprocess.call(
+        command, 
+        cwd=target,
+        shell=True
+    )
+
+    # Inform the user whether install is succes:
+    if ret != 0:
+        click.echo(click.style("ERROR", fg="red") + ": An error has occurred during installation of Litex.")
+        click.echo("Check the error message from the process. Possibly rerun the command with elevated permissions if the error is 'Permission denied'")    
+        return ret
+    
+    # Success
+    click.echo(click.style("INFO", fg="blue") + f": Successfully installed Litex in '{target}'")
+    return 0
+
+
+def _install_oss_cad_suite(target: str, user: bool, arch: str = None, os_:str = None):
     """Installs the Toolchain for ECP5 (oss-cad-suite)"""
+    # Determine the OS and architecture
     if not os_:
         os_ = platform.system().lower()
         click.echo(click.style("INFO", fg="blue") + f": Auto-detected os {os_}.")
@@ -30,6 +58,7 @@ def cli(user, arch, os_):
         else:
             arch = 'i386' if sys.maxsize > 2**32 else 'x64'
         click.echo(click.style("INFO", fg="blue") + f": Auto-detected architecture {arch}.")
+    
     # Check whether the combination if supported by oss-cad-suite
     if os_ == 'darwin':
         if not arch.lower() in ['arm64', 'x64']:
@@ -37,6 +66,7 @@ def cli(user, arch, os_):
     if os_ == 'windows':
         if not arch.lower() in ['x64']:
             click.echo(click.style("ERROR", fg="red") + f": Windows only supports x64 as architecture.")
+    
     # Download and install the selected files
     with tempfile.TemporaryDirectory() as tempdirname:
         # Download the toolchain from the source
@@ -50,9 +80,6 @@ def cli(user, arch, os_):
 
         # Determine the folder to unpack the tar-ball to
         click.echo(click.style("INFO", fg="blue") + f": Unpacking ...")
-        target = '/opt'
-        if user:
-            target = str(Path.home())
         
         # Extract the contents
         with tarfile.open(download) as archive:
@@ -80,3 +107,54 @@ def cli(user, arch, os_):
 
         # Done!
         click.echo(click.style("INFO", fg="blue") + f": OSS-CAD-Suite successfully installed")
+
+# def _install_openocd(target: str, user: bool):
+
+#     # Download and install the selected files
+#     with tempfile.TemporaryDirectory() as tempdirname:
+#         # Cloning OpenOCD
+#         click.echo(click.style("INFO", fg="blue") + f": Cloning OpenOCD ...")
+#         ret = subprocess.call(
+#             "git clone http://openocd.zylin.com/openocd", 
+#             cwd=tempdirname,
+#             shell=True
+#         )
+#         # Configure OpenOCD to include support for Raspberry Pi
+#         ret = subprocess.call(
+#             "./bootstrap && ./configure --enable-sysfsgpio --enable-bcm2835gpio && make", 
+#             cwd=os.path.join(tempdirname, "openocd"),
+#             shell=True
+#         )
+#         if user:
+#             ret = subprocess.call(
+#                 "make install", 
+#                 cwd=os.path.join(tempdirname, "openocd"),
+#                 shell=True
+#             ) 
+#         else:
+#             ret = subprocess.call(
+#                 "sudo make install", 
+#                 cwd=os.path.join(tempdirname, "openocd"),
+#                 shell=True
+#             )
+
+@click.command()
+@click.option('--user', is_flag=True)
+@click.option('--directory', '-d', help="Install the toolchain in specific location")
+@click.option(
+    '-a', '--architecture', 'arch',
+    type=click.Choice(['arm', 'arm64', 'x64'], case_sensitive=False),
+    help="Use specific architecture. Leave empty for auto-detect")
+@click.option(
+    '-os', '--os', 'os_',
+    type=click.Choice(['darwin', 'linux', 'windows'], case_sensitive=False),
+    help="Use specific os. Leave empty for auto-detect")
+def cli(user, directory, arch, os_):
+    """Installs the Toolchain for ECP5 (oss-cad-suite)"""
+    target = directory if directory else '/opt'
+    if user and not directory:
+        target = str(Path.home())
+    
+    # Install the components of the toolchain
+    _install_litex(target, user)
+    _install_oss_cad_suite(target, user, arch, os_)
